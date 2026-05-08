f5a45a1e9b8672f90ae014d42c2799275138b416
Hadi Rafi <hermesmerlin285@gmail.com>
2026-05-08 09:21:54 +0700

    menambahkan cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:xrpl2_plazaku/pages/search_page.dart';
+import 'package:xrpl2_plazaku/services/cart_service.dart';
+
+import '../utils/price_format.dart';
 
 class CartPage extends StatefulWidget {
-  const CartPage({super.key});
+  final CartService cartService;
+
+  const CartPage({super.key, required this.cartService});
 
   @override
   State<CartPage> createState() => _CartPageState();
@@ -10,6 +16,7 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
+    final cartProduct = widget.cartService.item;
     return Scaffold(
       backgroundColor: Color(0xFFF5F5F5),
       appBar: AppBar(
@@ -26,6 +33,180 @@ class _CartPageState extends State<CartPage> {
           ),
         ],
       ),
+      body: cartProduct.isEmpty
+          ? Center(child: Text('Cart empty'))
+          : Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SearchPage()),
+                      );
+                    },
+                    child: Container(
+                      height: 40,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 12),
+                      child: Row(
+                        children: [
+                          Icon(Icons.search, color: Colors.grey),
+                          SizedBox(width: 8),
+                          Text(
+                            'Search your product here',
+                            style: TextStyle(color: Colors.grey, fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: cartProduct.length,
+                      itemBuilder: (context, index) {
+                        final item = cartProduct[index];
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 12),
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(12),
+                                child: Image.asset(
+                                  item.product.image,
+                                  height: 100,
+                                  width: 100,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              SizedBox(width: 12),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      item.product.title,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(height: 6),
+                                    Text(
+                                      formatRupiah(item.product.price),
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                    Wrap(
+                                      crossAxisAlignment:
+                                          WrapCrossAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.orange,
+                                          size: 20,
+                                        ),
+                                        Text(
+                                          '${item.product.rating}',
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                        Text(
+                                          '(${item.product.review})',
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 12),
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 6,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                          color: Colors.grey.shade300,
+                                        ),
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          IconButton(
+                                            constraints: BoxConstraints(),
+                                            padding: EdgeInsets.zero,
+                                            onPressed: () {
+                                              setState(() {
+                                                if (item.quantity > 1) {
+                                                  item.quantity--;
+                                                } else {
+                                                  widget.cartService.removeCart(
+                                                    index,
+                                                  );
+                                                }
+                                              });
+                                            },
+                                            icon: Icon(Icons.remove, size: 18),
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.symmetric(
+                                              horizontal: 10,
+                                            ),
+                                            child: Text(
+                                              '${item.quantity}',
+                                              style: TextStyle(fontSize: 16),
+                                            ),
+                                          ),
+                                          IconButton(
+                                            constraints: BoxConstraints(),
+                                            padding: EdgeInsets.zero,
+                                            onPressed: () {
+                                              setState(() {
+                                                item.quantity++;
+                                              });
+                                            },
+                                            icon: Icon(Icons.add, size: 18),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Column(
+                                children: [
+                                  Checkbox(
+                                    value: item.product.isChosenCart,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        item.product.isChosenCart =
+                                            !item.product.isChosenCart;
+                                      });
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
